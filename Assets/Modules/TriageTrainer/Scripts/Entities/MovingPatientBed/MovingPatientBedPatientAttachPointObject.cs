1ed2ae4752079ef5b08b2af5a50f8d9bb66bb5bd
Jonghyeon Park <shapelayer.dev@gmail.com>
2026-05-06 17:03:01 +0900

    feat: add PetientAttachmentPoint into Moving PatientBed

---
+++ Assets/Modules/TriageTrainer/Scripts/Entities/MovingPatientBed/MovingPatientBedPatientAttachPointObject.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace TriageTrainer.Entity
+{
+  public class MovingPatientBedPatientAttachPointObject: MonoBehaviour
+  {
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+      DrawSceneViewPreview();
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+      DrawSceneViewPreview();
+    }
+
+    private void DrawSceneViewPreview()
+    {
+      Gizmos.color = new Color(0.85f, 1f, 0.2f, 0.9f);
+      Gizmos.DrawSphere(transform.position, 0.04f);
+    }
+#endif
+  }
+}
