a452b9373f054d9dde2b8d79a00aeaa1710e9319
강정훈 <kzkqkq@naver.com>
2026-08-04 19:27:39 +0900

    아이템 오타 수정 (gauze, gloves, NormalSaline1000ml, scissors)

--- Assets/Modules/TriageTrainer/Scripts/Items/Definitions/Gauze.cs
+++ Assets/Modules/TriageTrainer/Scripts/Items/Definitions/Gauze.cs
@@ -3,7 +3,7 @@ namespace TriageTrainer.ItemDefinitions
   public class Gauze : MedicalItem
   {
     public const string Identifier   = "gauze";
-    public const string DisplayName  = "면균거즈";
+    public const string DisplayName  = "멸균거즈";
     public const string Description  = "지혈 시 사용합니다.";
   }
 }
--- Assets/Modules/TriageTrainer/Scripts/Items/Definitions/Gloves.cs
+++ Assets/Modules/TriageTrainer/Scripts/Items/Definitions/Gloves.cs
@@ -3,7 +3,7 @@ namespace TriageTrainer.ItemDefinitions
   public class Gloves : MedicalItem
   {
     public const string Identifier   = "gloves";
-    public const string DisplayName  = "면균 장갑";
+    public const string DisplayName  = "멸균 장갑";
     public const string Description  = "";
   }
 }
